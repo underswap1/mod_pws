91bba9c3bdc77a487feafebc9df0e4ccb7c14b26
Underswap1 <levivanhorck@gmail.com>
2025-11-29 19:15:20 +0100

    registered new tools

--- src/main/java/dev/underswap/pws/PwsTools.java
+++ src/main/java/dev/underswap/pws/PwsTools.java
@@ -17,6 +17,16 @@ public class PwsTools {
         TOOLS.put("bone_axe", new AxeItem(ModToolMaterial.BONE, 5, -3.0F, new FabricItemSettings()));
         TOOLS.put("bone_shovel", new ShovelItem(ModToolMaterial.BONE, 1.5F, -3.0F, new FabricItemSettings()));
         TOOLS.put("bone_sword", new SwordItem(ModToolMaterial.BONE, 3, -2.4F, new FabricItemSettings()));
+        TOOLS.put("flint_axe", new AxeItem(ModToolMaterial.FLINT, 5, -3.0F, new FabricItemSettings()));
+        TOOLS.put("flint_pickaxe", new PickaxeItem(ModToolMaterial.FLINT, 1, -2.8F, new FabricItemSettings()));
+        TOOLS.put("flint_shovel", new ShovelItem(ModToolMaterial.FLINT, 1.5F, -3.0F, new FabricItemSettings()));
+        TOOLS.put("flint_sword", new SwordItem(ModToolMaterial.FLINT, 3, -2.4F, new FabricItemSettings()));
+        TOOLS.put("flint_hoe", new HoeItem(ModToolMaterial.FLINT, 1, -3.0F, new FabricItemSettings()));
+        TOOLS.put("sandstone_axe", new AxeItem(ModToolMaterial.SANDSTONE, 5, -3.0F, new FabricItemSettings()));
+        TOOLS.put("sandstone_pickaxe", new PickaxeItem(ModToolMaterial.SANDSTONE, 1, -2.8F, new FabricItemSettings()));
+        TOOLS.put("sandstone_shovel", new ShovelItem(ModToolMaterial.SANDSTONE, 1.5F, -3.0F, new FabricItemSettings()));
+        TOOLS.put("sandstone_sword", new SwordItem(ModToolMaterial.SANDSTONE, 3, -2.4F, new FabricItemSettings()));
+        TOOLS.put("obsidian_knife", new KnifeItem(ModToolMaterial.OBSIDIAN, 2.0F, -1.5F, new FabricItemSettings()));
     }
 
     public static void registerTools() {
